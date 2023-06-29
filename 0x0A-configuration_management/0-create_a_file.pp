e19c0d900ca11f6a419643df8613a2573611f3c1
Micaiah <micaiahadamson1993@gmail.com>
2023-06-29 02:17:37 -0700

    system devops

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-using puppet to create a file and making comment into the file.
+# creates a file in /tmp
+
 file { '/tmp/school':
-    ensure  => 'file',
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0744',
-    content => 'I love Puppet',
+  content =>'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
