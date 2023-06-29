09d342346c3f916c52e2d90cbf804348dd8a3f62
Micaiah <micaiahadamson1993@gmail.com>
2023-06-29 02:05:56 -0700

    puppet init and comment active

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 using puppet to create a file and making comment into the file.
-
-file{'tmp/school':
-ensure=> 'file',
-owner=> 'www-data',
-group=>'www-data',
-mode=>'0744',
-content=>'I love puppet'
+file { '/tmp/school':
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
 }
